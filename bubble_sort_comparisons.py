8d1eea8cad890c549388141fa6c06ebd6f30b8fe
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-14 16:16:38 +0100

    Fix syntax error in print statement for percentage difference

--- bubble_sort_comparisons.py
+++ bubble_sort_comparisons.py
@@ -10,6 +10,6 @@ def bubble_sort_comparison():
             
   print("bubble_sort time: ", bubble_sort_time)                          
   print("bubble_sort_finish_early time: ", bubble_sort_finish_early_time)
-  print("Percentage difference in time (with respect to bubble_sort): ,(bubble_sort_finish_early_time - bubble_sort_time)/ubble_sort_time)
+  print("Percentage difference in time (with respect to bubble_sort)" : ,(bubble_sort_finish_early_time - bubble_sort_time)/bubble_sort_time, "%")
   
 bubble_sort_comparison()
