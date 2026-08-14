36015f2412baa91e47cfe4abd7ea1dafdb0c2f82
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-14 14:38:51 +0100

    Create bubble_sort_comparions.py

---
+++ bubble_sort_comparions.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def bubble_sort_comparison():
+  a = np.random.randint(low=1,high=128,size=(100))
+  
+  print(a)
+  print(bubble_sort(a))
+  print(bubble_sort_finish_early(a))
+  %timeit bubble_sort(a)
+  %timeit bubble_sort_finish_early(a)
