b98012a6860d491073e3291954a286386f859ea4
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-14 16:15:33 +0100

    Refactor bubble_sort_comparison function
    Refactor bubble sort comparison function to improve time measurement and output format.

--- bubble_sort_comparisons.py
+++ bubble_sort_comparisons.py
@@ -5,14 +5,11 @@ from bubblesorts import bubble_sort, bubble_sort_finish_early
 def bubble_sort_comparison():
   a = np.random.randint(low=1,high=128,size=(100))
   
-  print(a)
-  print(bubble_sort(a))
-  print(bubble_sort_finish_early(a))
-  print("bubble_sort time:")
-  print(timeit.timeit(lambda: bubble_sort(a)))
-  print("bubble_sort_finish_early time:")
-  print(timeit.timeit(lambda: bubble_sort_finish_early(a)))
-  print("Percentage difference in time:")
-  print(((timeit.timeit(lambda: bubble_sort_finish_early(a)) - timeit.timeit(lambda: bubble_sort_finish_early(a)))/timeit.timeit(lambda: bubble_sort_finish_early(a)))*100, "%")
-
+  bubble_sort_time=timeit.timeit(lambda: bubble_sort(a), number=1)
+  bubble_sort_finish_early_time=timeit.timeit(lambda: bubble_sort_finish_early(a), number=1)
+            
+  print("bubble_sort time: ", bubble_sort_time)                          
+  print("bubble_sort_finish_early time: ", bubble_sort_finish_early_time)
+  print("Percentage difference in time (with respect to bubble_sort): ,(bubble_sort_finish_early_time - bubble_sort_time)/ubble_sort_time)
+  
 bubble_sort_comparison()
