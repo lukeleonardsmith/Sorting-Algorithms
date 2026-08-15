071d1efaf5c5c25049d99b7fd1bb1c2d8d45ccb4
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 11:31:26 +0100

    Fix import statement for bubble_sort module

--- bubble_sort_comparison.py
+++ bubble_sort_comparison.py
@@ -1,6 +1,6 @@
 import numpy as np
 import timeit
-from bubblesorts import bubble_sort, bubble_sort_finish_early
+from bubble_sorts import bubble_sort, bubble_sort_finish_early
 
 def bubble_sort_comparison():
   a = np.random.randint(low=1,high=128,size=(100))
