cb677defd4ff69655255b3ad6c1ccef61c81a52a
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 11:31:58 +0100

    Fix import statements and update print messages

--- examples.py
+++ examples.py
@@ -1,7 +1,7 @@
 import numpy as np
-from bubblesorts import bubble_sort, bubble_sort_finish_early
-from quicksorts import quick_sort, quick_sort_fast
-from mergesort import merge, merge_sort
+from bubble_sorts import bubble_sort, bubble_sort_finish_early
+from quick_sorts import quick_sort, quick_sort_fast
+from merge_sort import merge, merge_sort
 
 def examples():
   print("The following are each respective sorting algorithms sorting unique lists of 100 random integers.")
@@ -28,7 +28,7 @@ def bubble_sort_finish_early_example():
   a = np.random.randint(low=1,high=128,size=(100))
   print("The random list of numbers: ")
   print(a)
-  print("The sorted list of numbers with bubble sort (finish early): ")
+  print("The sorted list of numbers with bubble sort finish early: ")
   print(buble_sort_finish_early(a))
 
 def quick_sort_example():
@@ -42,7 +42,7 @@ def quick_sort_fast_example():
   a = np.random.randint(low=1,high=128,size=(100))
   print("The random list of numbers: ")
   print(a)
-  print("The sorted list of numbers with quick sort (fast): ")
+  print("The sorted list of numbers with quick sort fast: ")
   print(quick_sort_fast(a))
 
 def merge_sort_example():
