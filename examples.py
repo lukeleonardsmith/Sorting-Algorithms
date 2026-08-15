b0edfb879a3aaf3e954ce651f2a7dc0ed6758f0c
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 11:22:50 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,6 +1,7 @@
 import numpy as np
 from bubblesorts import bubble_sort, bubble_sort_finish_early
 from quicksorts import quick_sort, quick_sort_fast
+from mergesort import merge, merge_sort
 
 def examples():
   print("The following are each respective sorting algorithms sorting unique lists of 100 random integers.")
@@ -12,6 +13,8 @@ def examples():
   quick_sort_example()
   print()
   quick_sort_fast_example()
+  print()
+  merge_sort_example()
   
 
 def bubble_sort_example():
@@ -41,5 +44,12 @@ def quick_sort_fast_example():
   print(a)
   print("The sorted list of numbers with quick sort (fast): ")
   print(quick_sort_fast(a))
+
+def merge_sort_example():
+  a = np.random.randint(low=1,high=128,size=(100))
+  print("The random list of numbers: ")
+  print(a)
+  print("The sorted list of numbers with merge sort: ")
+  print(merge_sort(a))
 
 examples()
