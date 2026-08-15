6a41c0a96e80bc6ecc615fbbb2c9816ee5037646
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 11:33:37 +0100

    Fix variable name for quick_sort_fast time output

--- quick_sort_comparison.py
+++ quick_sort_comparison.py
@@ -9,7 +9,7 @@ def quick_sort_comparisons():
   quick_sort_fast_time=timeit.timeit(lambda: quick_sort_fast(a), number=1)
             
   print("quick_sort time: ", quick_sort_time)                          
-  print("quick_sort_fast time: ", bubble_sort_fast_time)
+  print("quick_sort_fast time: ", quick_sort_fast_time)
   print("Percentage difference in time (with respect to quick_sort): ", (quick_sort_fast_time - quick_sort_time)/quick_sort_time, "%")
   
 quick_sort_comparison()
