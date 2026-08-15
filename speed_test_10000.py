3ddfb01274a58a9b58ade02ad85f6f1d0b27719d
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 12:45:19 +0100

    Update speed_test_10000.py

--- speed_test_10000.py
+++ speed_test_10000.py
@@ -3,6 +3,7 @@ import timeit
 from bubble_sorts import bubble_sort, bubble_sort_finish_early
 from quick_sorts import quick_sort, quick_sort_fast
 from merge_sort import merge, merge_sort
+from insertion_sort import insertion_sort
 
 def speed_test_10000():
   a = np.random.randint(low=1,high=128,size=(10000))
@@ -12,11 +13,13 @@ def speed_test_10000():
   quick_sort_time=timeit.timeit(lambda: quick_sort(a), number=1)
   quick_sort_fast_time=timeit.timeit(lambda: quick_sort_fast(a), number=1)
   merge_sort_time=timeit.timeit(lambda: merge_sort(a), number=1)
+  insertion_sort_time=timeit.timeit(lambda: insertion_sort(a), number=1)
 
   print("bubble_sort time: ", bubble_sort_time)                          
   print("bubble_sort_finish_early time: ", bubble_sort_finish_early_time)
   print("quick_sort time: ", quick_sort_time)                          
   print("quick_sort_fast time: ", quick_sort_fast_time)
-  print("merge_sort time: ", merge_sort_time)                         
+  print("merge_sort time: ", merge_sort_time)
+  print("insertion_sort time: ", insertion_sort_time)
   
 speed_test_10000()
