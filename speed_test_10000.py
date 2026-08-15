324ac231107a4e12fa662624129106f0378bac1c
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-15 11:49:54 +0100

    Fix merge_sort function call in speed test

--- speed_test_10000.py
+++ speed_test_10000.py
@@ -11,7 +11,7 @@ def speed_test_10000():
   bubble_sort_finish_early_time=timeit.timeit(lambda: bubble_sort_finish_early(a), number=1) 
   quick_sort_time=timeit.timeit(lambda: quick_sort(a), number=1)
   quick_sort_fast_time=timeit.timeit(lambda: quick_sort_fast(a), number=1)
-  merge_sort_time=timeit.timeit(lambda: merge_sort_(a), number=1)
+  merge_sort_time=timeit.timeit(lambda: merge_sort(a), number=1)
 
   print("bubble_sort time: ", bubble_sort_time)                          
   print("bubble_sort_finish_early time: ", bubble_sort_finish_early_time)
