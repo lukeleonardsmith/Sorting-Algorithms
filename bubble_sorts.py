965b17ab930aacc67377f09e00bc4c4706bc5301
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-17 10:09:03 +0100

    Enhance bubble_sort with explanatory comments
    Added comments to clarify the purpose of loops and conditions in the bubble sort implementation.

--- bubble_sorts.py
+++ bubble_sorts.py
@@ -1,8 +1,8 @@
 def bubble_sort(a):
     b = list(a)
-    for n in range (0, len(a)-1):
+    for n in range (0, len(a)-1): # Loop from the start
         for k in range (0, len(a)-1):
-            if b[k] > b[k+1]:
+            if b[k] > b[k+1]: # Each iteration, if two elements are out of order, swap them.
                 c = list(b)
                 b[k] = c[k+1]
                 b[k+1] = c[k]
@@ -17,7 +17,7 @@ def bubble_sort_finish_early(a):
     iteration_before = []
     iteration_after = [1]
     for n in range (0, len(a)-1):
-        if iteration_after == iteration_before:
+        if iteration_after == iteration_before: # If there has been no change between two iterations, then the list is sorted, hence we can stop the algorithm.
             break
         else:
             iteration_before = list(b)
