8da10eec23fcaa725071df04b9a22fd8b575a5a6
lukeleonardsmith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-17 17:03:35 +0100

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,16 +1,12 @@
 def insertion_sort(a):
-    """Sort a list using Insertion Sort
-
-    input: list a
-    output: rearrangement of the list a in ascending order
-    """
+    
     b = list(a)  # Create a copy to avoid overwriting the contents of a
     n = len(a)
-    for k in range(1, n):
+    for k in range(1, n): # Loop for all elements in the list
         x = b[k]
         j = k - 1
-        while (j >= 0) and (b[j] > x):
+        while (j >= 0) and (b[j] > x): # Find the point in the list where every element to the right is greater than the chosen element
             b[j + 1] = b[j]
             j = j - 1
-        b[j + 1] = x
+        b[j + 1] = x # Insert the element here
     return b
