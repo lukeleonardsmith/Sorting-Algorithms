21e3b5d2ffa2226a38fe3c0c6f8432203cf30fae
Luke Smith <67478783+lukeleonardsmith@users.noreply.github.com>
2026-08-31 14:51:24 +0100

    Modify input sizes and improve plot labels
    Reduced input sizes for sorting algorithms and adjusted the number of iterations for time measurement. Updated plot labels for better readability.

--- runtime_vs_input_size.py
+++ runtime_vs_input_size.py
@@ -10,7 +10,7 @@ from merge_sort import merge, merge_sort
 from insertion_sort import insertion_sort
 
 def size_plot():
-  input_size = np.array([100, 500, 1000, 2000])
+  input_size = np.array([100, 500])
   bubble_sort_time = np.array([])
   bubble_sort_finish_early_time = np.array([])
   quick_sort_time = np.array([])
@@ -21,19 +21,19 @@ def size_plot():
   for i in input_size:
     a = np.random.randint(low=1,high=10000,size=(i))
     
-    bubble_sort_time = np.append(bubble_sort_time, timeit.timeit(lambda: bubble_sort(a), number=100)/100)
-    bubble_sort_finish_early_time = np.append(bubble_sort_finish_early_time, timeit.timeit(lambda: bubble_sort_finish_early(a), number=100)/100)
-    quick_sort_time = np.append(quick_sort_time, timeit.timeit(lambda: quick_sort(a), number=100)/100)
-    quick_sort_fast_time = np.append(quick_sort_fast_time, timeit.timeit(lambda: quick_sort_fast(a), number=100)/100)
-    merge_sort_time = np.append(merge_sort_time, timeit.timeit(lambda: merge_sort(a), number=100)/100)
-    insertion_sort_time = np.append(insertion_sort_time, timeit.timeit(lambda: insertion_sort(a), number=100)/100)
+    bubble_sort_time = np.append(bubble_sort_time, timeit.timeit(lambda: bubble_sort(a), number=10)/10)
+    bubble_sort_finish_early_time = np.append(bubble_sort_finish_early_time, timeit.timeit(lambda: bubble_sort_finish_early(a), number=10)/10)
+    quick_sort_time = np.append(quick_sort_time, timeit.timeit(lambda: quick_sort(a), number=10)/10)
+    quick_sort_fast_time = np.append(quick_sort_fast_time, timeit.timeit(lambda: quick_sort_fast(a), number=10)/10)
+    merge_sort_time = np.append(merge_sort_time, timeit.timeit(lambda: merge_sort(a), number=10)/10)
+    insertion_sort_time = np.append(insertion_sort_time, timeit.timeit(lambda: insertion_sort(a), number=10)/10)
 
-  plt.plot(input_size, bubble_sort_time, label = Bubble_Sort)
-  plt.plot(input_size, bubble_sort_finish_early_time, label = Bubble_Sort_Finish_Early)
-  plt.plot(input_size, quick_sort_time, label = Quikc_Sort)
-  plt.plot(input_size, quick_sort_fast_time, label = Quick_sort_Fast)
-  plt.plot(input_size, merge_sort_time, label = Merge_Sort)
-  plt.plot(input_size, insertion_sort_time, label = Insertion_Sort)
+  plt.plot(input_size, bubble_sort_time, label = 'Bubble Sort')
+  plt.plot(input_size, bubble_sort_finish_early_time, label = 'Bubble Sort Finish Early')
+  plt.plot(input_size, quick_sort_time, label = 'Quick Sort')
+  plt.plot(input_size, quick_sort_fast_time, label = 'Quick sort Fast')
+  plt.plot(input_size, merge_sort_time, label = 'Merge Sort')
+  plt.plot(input_size, insertion_sort_time, label = 'Insertion Sort')
   plt.xlabel("Input Size")
   plt.ylabel("Time taken")
   plt.title("Runtime vs Input Size")
